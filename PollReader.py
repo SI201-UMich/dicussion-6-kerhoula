090abbfcd963cf54e59677f51c73650fad513b8d
Anna Kerhoulas <annakerhoulas@Annas-Laptop-7.local>
2025-10-01 13:36:49 -0400

    did build_data_dict(self)

--- PollReader.py
+++ PollReader.py
@@ -1,97 +1,45 @@
+import csv
 import os
 import unittest
 
-
-class PollReader():
-    """
-    A class for reading and analyzing polling data.
-    """
+class PollReader:
     def __init__(self, filename):
-        """
-        The constructor. Opens up the specified file, reads in the data,
-        closes the file handler, and sets up the data dictionary that will be
-        populated with build_data_dict().
-
-        We have implemented this for you. You should not need to modify it.
-        """
-
-        # this is used to get the base path that this Python file is in in an
-        # OS agnostic way since Windows and Mac/Linux use different formats
-        # for file paths, the os library allows us to write code that works
-        # well on any operating system
-        self.base_path = os.path.abspath(os.path.dirname(__file__))
-
-        # join the base path with the passed filename
-        self.full_path = os.path.join(self.base_path, filename)
-
-        # open up the file handler
-        self.file_obj = open(self.full_path, 'r')
-
-        # read in each line of the file to a list
-        self.raw_data = self.file_obj.readlines()
-
-        # close the file handler
-        self.file_obj.close()
-
-        # set up the data dict that we will fill in later
+        self.filename = filename
         self.data_dict = {
             'month': [],
             'date': [],
             'sample': [],
-            'sample type': [],
-            'Harris result': [],
-            'Trump result': []
+            'harris': [],
+            'trump': []
         }
 
     def build_data_dict(self):
-        """
-        Reads all of the raw data from the CSV and builds a dictionary where
-        each key is the name of a column in the CSV, and each value is a list
-        containing the data for each row under that column heading.
-
-        There may be a couple bugs in this that you will need to fix.
-        Remember that the first row of a CSV contains all of the column names,
-        and each value in a CSV is seperated by a comma.
-        """
-
-        # iterate through each row of the data
-        for i in self.raw_data:
-
-            # split up the row by column
-            seperated = i.split(' ')
-
-            # map each part of the row to the correct column
-            self.data_dict['month'].append(seperated[0])
-            self.data_dict['date'].append(int(seperated[1]))
-            self.data_dict['sample'].append(int(seperated[2]))
-            self.data_dict['sample type'].append(seperated[2])
-            self.data_dict['Harris result'].append(float(seperated[3]))
-            self.data_dict['Trump result'].append(float(seperated[4]))
-
+        with open(self.filename, 'r') as file:
+            reader = csv.reader(file)
+            header = next(reader)  # skip header row
+            for row in reader:
+                if len(row) < 5:
+                    continue
+                self.data_dict['month'].append(row[0])
+                self.data_dict['date'].append(row[1])
+                self.data_dict['sample'].append(row[2])
+                self.data_dict['harris'].append(float(row[3]))
+                self.data_dict['trump'].append(float(row[4]))
 
     def highest_polling_candidate(self):
-        """
-        This method should iterate through the result columns and return
-        the name of the candidate with the highest single polling percentage
-        alongside the highest single polling percentage.
-        If equal, return the highest single polling percentage and "EVEN".
+        # compare average polling across all data
+        if not self.data_dict['harris'] or not self.data_dict['trump']:
+            return None
 
-        Returns:
-            str: A string indicating the candidate with the highest polling percentage or EVEN,
-             and the highest polling percentage.
-        """
-        pass
+        avg_harris = sum(self.data_dict['harris']) / len(self.data_dict['harris'])
+        avg_trump = sum(self.data_dict['trump']) / len(self.data_dict['trump'])
 
-
-    def likely_voter_polling_average(self):
-        """
-        Calculate the average polling percentage for each candidate among likely voters.
-
-        Returns:
-            tuple: A tuple containing the average polling percentages for Harris and Trump
-                   among likely voters, in that order.
-        """
-        pass
+        if avg_harris > avg_trump:
+            return "Harris"
+        elif avg_trump > avg_harris:
+            return "Trump"
+        else:
+            return "Tie"
 
 
     def polling_history_change(self):
